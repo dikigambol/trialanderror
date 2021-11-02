2534e6d495e6e9c651cea042bb8ad4e84a015d32
Zen Bgs <49084336+ancientlord123@users.noreply.github.com>
2021-11-02 10:31:05 +0700

    template pdf

---
+++ src/halaman/templatePDF.js
@@ -0,0 +1,136 @@
+<html>
+<head><meta http-equiv=Content-Type content="text/html; charset=UTF-8">
+<style type="text/css">
+
+span.cls_002{font-family:"DejaVu Serif Condensed Bold",serif;font-size:11.1px;color:rgb(0,0,0);font-weight:bold;font-style:normal;text-decoration: none}
+div.cls_002{font-family:"DejaVu Serif Condensed Bold",serif;font-size:11.1px;color:rgb(0,0,0);font-weight:bold;font-style:normal;text-decoration: none}
+span.cls_003{font-family:"DejaVu Serif Condensed",serif;font-size:10.6px;color:rgb(0,0,0);font-weight:normal;font-style:normal;text-decoration: none}
+div.cls_003{font-family:"DejaVu Serif Condensed",serif;font-size:10.6px;color:rgb(0,0,0);font-weight:normal;font-style:normal;text-decoration: none}
+span.cls_004{font-family:"DejaVu Serif Condensed",serif;font-size:8.8px;color:rgb(0,0,0);font-weight:normal;font-style:normal;text-decoration: none}
+div.cls_004{font-family:"DejaVu Serif Condensed",serif;font-size:8.8px;color:rgb(0,0,0);font-weight:normal;font-style:normal;text-decoration: none}
+span.cls_005{font-family:"DejaVu Serif Condensed",serif;font-size:9.2px;color:rgb(0,0,0);font-weight:normal;font-style:normal;text-decoration: none}
+div.cls_005{font-family:"DejaVu Serif Condensed",serif;font-size:9.2px;color:rgb(0,0,0);font-weight:normal;font-style:normal;text-decoration: none}
+span.cls_006{font-family:"DejaVu Serif Condensed Bold",serif;font-size:9.0px;color:rgb(0,0,0);font-weight:bold;font-style:normal;text-decoration: none}
+div.cls_006{font-family:"DejaVu Serif Condensed Bold",serif;font-size:9.0px;color:rgb(0,0,0);font-weight:bold;font-style:normal;text-decoration: none}
+span.cls_007{font-family:"DejaVu Serif Condensed Bold",serif;font-size:11.0px;color:rgb(0,0,0);font-weight:bold;font-style:normal;text-decoration: none}
+div.cls_007{font-family:"DejaVu Serif Condensed Bold",serif;font-size:11.0px;color:rgb(0,0,0);font-weight:bold;font-style:normal;text-decoration: none}
+span.cls_008{font-family:"DejaVu Serif Condensed",serif;font-size:8.3px;color:rgb(0,0,0);font-weight:normal;font-style:normal;text-decoration: none}
+div.cls_008{font-family:"DejaVu Serif Condensed",serif;font-size:8.3px;color:rgb(0,0,0);font-weight:normal;font-style:normal;text-decoration: none}
+span.cls_009{font-family:"DejaVu Serif Condensed Bold",serif;font-size:8.3px;color:rgb(0,0,0);font-weight:bold;font-style:normal;text-decoration: none}
+div.cls_009{font-family:"DejaVu Serif Condensed Bold",serif;font-size:8.3px;color:rgb(0,0,0);font-weight:bold;font-style:normal;text-decoration: none}
+span.cls_011{font-family:"DejaVu Serif Condensed",serif;font-size:9.6px;color:rgb(0,0,0);font-weight:normal;font-style:normal;text-decoration: none}
+div.cls_011{font-family:"DejaVu Serif Condensed",serif;font-size:9.6px;color:rgb(0,0,0);font-weight:normal;font-style:normal;text-decoration: none}
+span.cls_013{font-family:"DejaVu Serif Condensed",serif;font-size:9.6px;color:rgb(0,0,0);font-weight:normal;font-style:normal;text-decoration: underline}
+div.cls_013{font-family:"DejaVu Serif Condensed",serif;font-size:9.6px;color:rgb(0,0,0);font-weight:normal;font-style:normal;text-decoration: none}
+
+</style>
+
+</head>
+<body>
+<div style="position:absolute;left:50%;margin-left:-297px;top:0px;width:595px;height:841px;border-style:outset;overflow:hidden">
+<div style="position:absolute;left:0px;top:0px">
+<img src="7801c746-3b8b-11ec-a980-0cc47a792c0a_id_7801c746-3b8b-11ec-a980-0cc47a792c0a_files/background1.jpg" width=595 height=841></div>
+<div style="position:absolute;left:153.94px;top:74.20px" class="cls_002"><span class="cls_002">INSTITUT TEKNOLOGI DAN BISNIS ASIA MALANG</span></div>
+<div style="position:absolute;left:192.04px;top:91.10px" class="cls_002"><span class="cls_002">FAKULTAS TEKNOLOGI DAN DESAIN</span></div>
+<div style="position:absolute;left:95.50px;top:107.97px" class="cls_003"><span class="cls_003">Jl. Soekarno-Hatta, Rembuksari 1A Malang Telp.(0341) 478877 Fax.(0341) 4345225</span></div>
+<div style="position:absolute;left:237.88px;top:172.43px" class="cls_002"><span class="cls_002">KARTU HASIL STUDI</span></div>
+<div style="position:absolute;left:27.68px;top:215.34px" class="cls_004"><span class="cls_004">Nama Mahasiswa</span></div>
+<div style="position:absolute;left:136.19px;top:215.07px" class="cls_005"><span class="cls_005">:  ZAIN BAGUS DWI ASMORO</span></div>
+<div style="position:absolute;left:374.91px;top:215.07px" class="cls_005"><span class="cls_005">Tahun Angkatan</span></div>
+<div style="position:absolute;left:472.57px;top:215.07px" class="cls_005"><span class="cls_005">:</span></div>
+<div style="position:absolute;left:483.42px;top:215.07px" class="cls_005"><span class="cls_005">2020</span></div>
+<div style="position:absolute;left:27.68px;top:229.14px" class="cls_005"><span class="cls_005">NIM</span></div>
+<div style="position:absolute;left:136.19px;top:229.14px" class="cls_005"><span class="cls_005">:</span></div>
+<div style="position:absolute;left:147.04px;top:229.14px" class="cls_005"><span class="cls_005">20201219</span></div>
+<div style="position:absolute;left:374.91px;top:229.14px" class="cls_005"><span class="cls_005">Semester</span></div>
+<div style="position:absolute;left:472.57px;top:229.14px" class="cls_005"><span class="cls_005">:</span></div>
+<div style="position:absolute;left:483.42px;top:229.14px" class="cls_005"><span class="cls_005">02</span></div>
+<div style="position:absolute;left:27.68px;top:243.21px" class="cls_005"><span class="cls_005">Program Studi</span></div>
+<div style="position:absolute;left:136.19px;top:243.21px" class="cls_005"><span class="cls_005">:  TEKNIK INFORMATIKA</span></div>
+<div style="position:absolute;left:374.91px;top:243.21px" class="cls_005"><span class="cls_005">Tahun Akademik</span></div>
+<div style="position:absolute;left:472.57px;top:243.21px" class="cls_005"><span class="cls_005">:</span></div>
+<div style="position:absolute;left:483.42px;top:243.21px" class="cls_005"><span class="cls_005">2020/2021</span></div>
+<div style="position:absolute;left:27.68px;top:257.29px" class="cls_005"><span class="cls_005">Konsentrasi</span></div>
+<div style="position:absolute;left:136.19px;top:257.29px" class="cls_005"><span class="cls_005">:</span></div>
+<div style="position:absolute;left:147.04px;top:257.29px" class="cls_005"><span class="cls_005">-</span></div>
+<div style="position:absolute;left:36.13px;top:291.54px" class="cls_006"><span class="cls_006">No</span></div>
+<div style="position:absolute;left:72.62px;top:291.54px" class="cls_006"><span class="cls_006">Kode Mata kuliah</span></div>
+<div style="position:absolute;left:219.41px;top:291.54px" class="cls_006"><span class="cls_006">Nama Mata Kuliah</span></div>
+<div style="position:absolute;left:369.97px;top:290.24px" class="cls_007"><span class="cls_007">SKS</span></div>
+<div style="position:absolute;left:421.99px;top:291.54px" class="cls_006"><span class="cls_006">Nilai</span></div>
+<div style="position:absolute;left:473.57px;top:291.54px" class="cls_006"><span class="cls_006">Huruf</span></div>
+<div style="position:absolute;left:523.09px;top:291.54px" class="cls_006"><span class="cls_006">SKS x N</span></div>
+<div style="position:absolute;left:40.17px;top:312.51px" class="cls_008"><span class="cls_008">1</span></div>
+<div style="position:absolute;left:97.56px;top:312.51px" class="cls_008"><span class="cls_008">MUF-02</span></div>
+<div style="position:absolute;left:171.73px;top:312.51px" class="cls_008"><span class="cls_008">Technology in Practice 2</span></div>
+<div style="position:absolute;left:379.05px;top:312.51px" class="cls_008"><span class="cls_008">2</span></div>
+<div style="position:absolute;left:424.67px;top:312.51px" class="cls_008"><span class="cls_008">4.00</span></div>
+<div style="position:absolute;left:484.46px;top:312.51px" class="cls_008"><span class="cls_008">A</span></div>
+<div style="position:absolute;left:533.11px;top:312.51px" class="cls_008"><span class="cls_008">8.00</span></div>
+<div style="position:absolute;left:40.17px;top:331.39px" class="cls_008"><span class="cls_008">2</span></div>
+<div style="position:absolute;left:97.66px;top:331.39px" class="cls_008"><span class="cls_008">MUT-04</span></div>
+<div style="position:absolute;left:171.73px;top:331.39px" class="cls_008"><span class="cls_008">Pemrograman Berorientasi Obyek (Java)</span></div>
+<div style="position:absolute;left:379.05px;top:331.39px" class="cls_008"><span class="cls_008">3</span></div>
+<div style="position:absolute;left:424.67px;top:331.39px" class="cls_008"><span class="cls_008">4.00</span></div>
+<div style="position:absolute;left:484.46px;top:331.39px" class="cls_008"><span class="cls_008">A</span></div>
+<div style="position:absolute;left:530.76px;top:331.39px" class="cls_008"><span class="cls_008">12.00</span></div>
+<div style="position:absolute;left:40.17px;top:350.27px" class="cls_008"><span class="cls_008">3</span></div>
+<div style="position:absolute;left:97.66px;top:350.27px" class="cls_008"><span class="cls_008">MUT-01</span></div>
+<div style="position:absolute;left:171.73px;top:350.27px" class="cls_008"><span class="cls_008">Metode Numerik</span></div>
+<div style="position:absolute;left:379.05px;top:350.27px" class="cls_008"><span class="cls_008">3</span></div>
+<div style="position:absolute;left:424.67px;top:350.27px" class="cls_008"><span class="cls_008">3.67</span></div>
+<div style="position:absolute;left:483.21px;top:350.27px" class="cls_008"><span class="cls_008">A-</span></div>
+<div style="position:absolute;left:530.76px;top:350.27px" class="cls_008"><span class="cls_008">11.01</span></div>
+<div style="position:absolute;left:40.17px;top:369.15px" class="cls_008"><span class="cls_008">4</span></div>
+<div style="position:absolute;left:97.66px;top:369.15px" class="cls_008"><span class="cls_008">MUT-07</span></div>
+<div style="position:absolute;left:171.73px;top:369.15px" class="cls_008"><span class="cls_008">Desain dan Rekayasa Perangkat Lunak</span></div>
+<div style="position:absolute;left:379.05px;top:369.15px" class="cls_008"><span class="cls_008">4</span></div>
+<div style="position:absolute;left:424.67px;top:369.15px" class="cls_008"><span class="cls_008">3.33</span></div>
+<div style="position:absolute;left:481.31px;top:369.15px" class="cls_008"><span class="cls_008">B+</span></div>
+<div style="position:absolute;left:530.76px;top:369.15px" class="cls_008"><span class="cls_008">13.32</span></div>
+<div style="position:absolute;left:40.17px;top:388.04px" class="cls_008"><span class="cls_008">5</span></div>
+<div style="position:absolute;left:97.66px;top:388.04px" class="cls_008"><span class="cls_008">MUT-15</span></div>
+<div style="position:absolute;left:171.73px;top:388.04px" class="cls_008"><span class="cls_008">Animasi 2D</span></div>
+<div style="position:absolute;left:379.05px;top:388.04px" class="cls_008"><span class="cls_008">3</span></div>
+<div style="position:absolute;left:424.67px;top:388.04px" class="cls_008"><span class="cls_008">3.67</span></div>
+<div style="position:absolute;left:483.21px;top:388.04px" class="cls_008"><span class="cls_008">A-</span></div>
+<div style="position:absolute;left:530.76px;top:388.04px" class="cls_008"><span class="cls_008">11.01</span></div>
+<div style="position:absolute;left:40.17px;top:406.92px" class="cls_008"><span class="cls_008">6</span></div>
+<div style="position:absolute;left:97.66px;top:406.92px" class="cls_008"><span class="cls_008">MUT-03</span></div>
+<div style="position:absolute;left:171.73px;top:406.92px" class="cls_008"><span class="cls_008">Analisis Algoritma dan Struktur Data</span></div>
+<div style="position:absolute;left:379.05px;top:406.92px" class="cls_008"><span class="cls_008">3</span></div>
+<div style="position:absolute;left:424.67px;top:406.92px" class="cls_008"><span class="cls_008">4.00</span></div>
+<div style="position:absolute;left:484.46px;top:406.92px" class="cls_008"><span class="cls_008">A</span></div>
+<div style="position:absolute;left:530.76px;top:406.92px" class="cls_008"><span class="cls_008">12.00</span></div>
+<div style="position:absolute;left:40.17px;top:425.80px" class="cls_008"><span class="cls_008">7</span></div>
+<div style="position:absolute;left:97.16px;top:425.80px" class="cls_008"><span class="cls_008">MDU-06</span></div>
+<div style="position:absolute;left:171.73px;top:425.80px" class="cls_008"><span class="cls_008">Interaksi Manusia dan Komputer</span></div>
+<div style="position:absolute;left:379.05px;top:425.80px" class="cls_008"><span class="cls_008">2</span></div>
+<div style="position:absolute;left:424.67px;top:425.80px" class="cls_008"><span class="cls_008">4.00</span></div>
+<div style="position:absolute;left:484.46px;top:425.80px" class="cls_008"><span class="cls_008">A</span></div>
+<div style="position:absolute;left:533.11px;top:425.80px" class="cls_008"><span class="cls_008">8.00</span></div>
+<div style="position:absolute;left:30.76px;top:446.37px" class="cls_009"><span class="cls_009">Total</span></div>
+<div style="position:absolute;left:377.82px;top:447.28px" class="cls_009"><span class="cls_009">20</span></div>
+<div style="position:absolute;left:433.24px;top:447.28px" class="cls_008"><span class="cls_008">-</span></div>
+<div style="position:absolute;left:487.46px;top:447.28px" class="cls_008"><span class="cls_008">-</span></div>
+<div style="position:absolute;left:531.33px;top:447.28px" class="cls_009"><span class="cls_009">75.34</span></div>
+<div style="position:absolute;left:245.02px;top:466.90px" class="cls_009"><span class="cls_009">IP Semester</span></div>
+<div style="position:absolute;left:532.34px;top:466.90px" class="cls_009"><span class="cls_009">3.77</span></div>
+<div style="position:absolute;left:243.38px;top:485.78px" class="cls_009"><span class="cls_009">IP Kumulatif</span></div>
+<div style="position:absolute;left:532.34px;top:485.78px" class="cls_009"><span class="cls_009">3.78</span></div>
+<div style="position:absolute;left:215.74px;top:504.66px" class="cls_009"><span class="cls_009">SKS yang Telah Ditempuh</span></div>
+<div style="position:absolute;left:536.21px;top:504.66px" class="cls_009"><span class="cls_009">40</span></div>
+<div style="position:absolute;left:190.35px;top:525.19px" class="cls_009"><span class="cls_009">Beban Maksimal SKS Semester Depan</span></div>
+<div style="position:absolute;left:537.92px;top:526.16px" class="cls_009"><span class="cls_009">24</span></div>
+<div style="position:absolute;left:384.40px;top:568.86px" class="cls_011"><span class="cls_011">Malang, 02 November 2021</span></div>
+<div style="position:absolute;left:121.94px;top:583.67px" class="cls_011"><span class="cls_011">Mengetahui,</span></div>
+<div style="position:absolute;left:99.90px;top:598.48px" class="cls_011"><span class="cls_011">Kepala Program Studi,</span></div>
+<div style="position:absolute;left:419.35px;top:598.48px" class="cls_011"><span class="cls_011">Dosen Wali,</span></div>
+<div style="position:absolute;left:61.80px;top:659.35px" class="cls_013"><span class="cls_013">JAENAL ARIFIN , S.Kom. M.M. , M.Kom</span></div>
+<div style="position:absolute;left:361.32px;top:659.35px" class="cls_013"><span class="cls_013">TITANIA DWI ANDINI S.Kom., M.Kom</span></div>
+<div style="position:absolute;left:108.18px;top:674.16px" class="cls_011"><span class="cls_011">NIDN.0709117502</span></div>
+<div style="position:absolute;left:404.03px;top:674.16px" class="cls_011"><span class="cls_011">NIDN.0718047701</span></div>
+</div>
+
+</body>
+</html>
